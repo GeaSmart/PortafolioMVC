84eaf218d7ae1738bf73b738050a2ccf02986c1c
Gerson Azabache <gerson_dunk@hotmail.com>
2022-03-08 17:31:42 -0500

    adding entities

---
+++ Model/Experiencia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Table("Experiencias")]
+    public class Experiencia
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public byte Tipo { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Titulo { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Desde { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Hasta { get; set; }
+        [Column(TypeName = "text")]
+        public string Descripcion { get; set; }
+
+        //propiedades de navegacion
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ Model/Usuario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Nombres { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Apellidos { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Direccion { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Ciudad { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Pais { get; set; }
+        public string Telefono { get; set; }
+        public string Linkedin { get; set; }
+        public string Github { get; set; }
+        public string Youtube { get; set; }
+        public string Twitter { get; set; }
+
+        //propiedades de navegación
+        public List<Experiencia> Experiencias{ get; set; }
+        public List<Testimonio> Testimonios { get; set; }
+        public List<Habilidad> Habilidades { get; set; }
+
+    }
+}
