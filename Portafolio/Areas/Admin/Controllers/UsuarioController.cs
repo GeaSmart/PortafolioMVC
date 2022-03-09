1a90e802754e1dfcacbd344860bcdc07cca9c048
Gerson Azabache <gerson_dunk@hotmail.com>
2022-03-09 12:57:36 -0500

    responde model added, access logic in progress for sessions

--- Portafolio/Areas/Admin/Controllers/UsuarioController.cs
+++ Portafolio/Areas/Admin/Controllers/UsuarioController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Common;
+using Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +16,35 @@ namespace Portafolio.Areas.Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ResponseModel> Acceder(string email, string password)
+        {
+            var response = new ResponseModel();
+            var hashedPassword = HashHelper.MD5(password);
+            try
+            {
+                using (var context = new ApplicationDbContext())
+                {
+                    var usuario = await context.Usuarios.Where(x => x.Email == email && x.Password == hashedPassword).FirstOrDefaultAsync();
+
+                    if (usuario != null)
+                    {
+                        SessionHelper.AddUserToSession(usuario.Id.ToString());
+                        response.SetResponse(true);
+                    }
+                    else
+                    {
+                        response.SetResponse(false, "Credenciales incorrectas");
+                    }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            return response;
         }
     }
 }
