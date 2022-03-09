ce969a45819095709f420fafab14eab401bded3d
Gerson Azabache <gerson_dunk@hotmail.com>
2022-03-09 09:59:59 -0500

    backend layouts added

---
+++ Portafolio/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portafolio.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            // Eliminar la sesion actual
+            return Redirect("~/");
+        }
+    }
+}
