a0cf5807c1647f8fb4ec7bfce9497ae1a053b98d
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2022-03-25 18:49:36 -0500

    testimonios completed

--- Model/Testimonio.cs
+++ Model/Testimonio.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,5 +29,139 @@ namespace Model
 
         //propiedades de navegación
         public Usuario Usuario { get; set; }
+
+        #region Métodos
+
+        public Testimonio Obtener(int id)
+        {
+            var testimonio = new Testimonio();
+
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    testimonio = ctx.Testimonios.Where(x => x.Id == id)
+                                                 .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return testimonio;
+        }
+
+        public ResponseModel Guardar()
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    if (this.Id > 0) ctx.Entry(this).State = EntityState.Modified;
+                    else ctx.Entry(this).State = EntityState.Added;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        public ResponseModel Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    this.Id = id;
+                    ctx.Entry(this).State = EntityState.Deleted;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        public MyGridResponde Listar(MyGrid grid, int UsuarioId)
+        {
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+
+                    grid.Inicializar();
+
+                    var query = ctx.Testimonios.Where(x => x.UsuarioId == UsuarioId);
+
+                    // Ordenamiento
+                    if (grid.columna == "Id")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Id)
+                                                             : query.OrderBy(x => x.Id);
+                    }
+
+                    if (grid.columna == "Nombre")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Nombre)
+                                                             : query.OrderBy(x => x.Nombre);
+                    }
+
+                    if (grid.columna == "Ip")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Ip)
+                                                             : query.OrderBy(x => x.Ip);
+                    }
+                    if (grid.columna == "Comentario")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Comentario)
+                                                             : query.OrderBy(x => x.Comentario);
+                    }
+                    if (grid.columna == "Fecha")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Fecha)
+                                                             : query.OrderBy(x => x.Fecha);
+                    }
+
+                    // Id, Nombre, Dominio
+
+                    var testimonios = query.Skip(grid.pagina)
+                                            .Take(grid.limite)
+                                             .ToList();
+
+                    var total = query.Count();
+
+                    grid.SetData(testimonios, total);
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return grid.responde();
+        }
+
+        #endregion
     }
 }
--- Portafolio/Areas/Admin/Controllers/TestimoniosController.cs
+++ Portafolio/Areas/Admin/Controllers/TestimoniosController.cs
@@ -1,4 +1,6 @@
-﻿using Portafolio.Areas.Admin.Filters;
+﻿using Common;
+using Model;
+using Portafolio.Areas.Admin.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,48 @@ namespace Portafolio.Areas.Admin.Controllers
     [Autenticado]
     public class TestimoniosController : Controller
     {
-        // GET: Admin/Testimonios
+        private Testimonio testimonio = new Testimonio();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult Listar(MyGrid grid)
+        {
+            return Json(testimonio.Listar(grid, SessionHelper.GetUser()));
+        }
+
+        public ActionResult Crud(int id)//no se puede desde el backoffice añadir testimonios, estos vienen desde el frontoffice
+        {
+            testimonio = testimonio.Obtener(id);            
+            return View(testimonio);
+        }
+
+        public JsonResult Guardar(Testimonio model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/admin/testimonios/");
+                }
+            }
+            return Json(rm);
+        }
+
+        public JsonResult Eliminar(int id)
+        {
+            var rm = testimonio.Eliminar(id);
+
+            //if (rm.response)
+            //{
+            //    rm.href = "self";
+            //}
+
+            return Json(rm);
         }
     }
 }
